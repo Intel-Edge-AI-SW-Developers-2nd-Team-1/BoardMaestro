5e49ede923ec672d659d7d00387f2b84b008a1ed
acs6089 <acs6089@gmail.com>
2023-11-22 20:03:21 +0900

    updated hand_landmark_estimation.py & updated hand_pattern_recognition_module.py & hand_pose_estimation_show_module.py & image_inferencing_module.py by User3198352

--- Hand_Pose_Estimation/hand_landmark_estimation.py
+++ Hand_Pose_Estimation/hand_landmark_estimation.py
@@ -1,89 +1,25 @@
+import time
 import cv2
 import numpy as np
-import time
 
 import mediapipe as mp
 from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
-
-from mediapipe import solutions
-from mediapipe.framework.formats import landmark_pb2
 
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from hand_pattern_recognition_module import HandPatternRecognition
-
+from hand_pose_estimation_show_module import HandPoseEstimationShow
+from image_inferencing_module import ImageInferencing
 
 # define HandLandmarkers elements
-model_path = 'C:\pywork\hand_landmarker.task'
+model_path = 'C:/pywork/hand_landmarker.task'
 
 BaseOptions = mp.tasks.BaseOptions
 HandLandmarker = mp.tasks.vision.HandLandmarker
 HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
 HandLandmarkerResult = mp.tasks.vision.HandLandmarkerResult
 VisionRunningMode = mp.tasks.vision.RunningMode
-
-'''
-def print_result(result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
-    #Create a hand landmarker instance with the live stream mode:
-    print('hand landmarker result: {}'.format(result))
-'''
-
-# define values for draw_landmarks_on_image
-MARGIN = 10  # pixels
-FONT_SIZE = 1
-FONT_THICKNESS = 1
-HANDEDNESS_TEXT_COLOR = (88, 205, 54) # vibrant green
-
-def draw_landmarks_on_image(rgb_image, detection_result):
-    '''to visualize the hand landmark detection results'''
-    hand_landmarks_list = detection_result.hand_landmarks
-    handedness_list = detection_result.handedness
-    annotated_image = np.copy(rgb_image)
-
-    # Loop through the detected hands to visualize.
-    for idx in range(len(hand_landmarks_list)):
-        hand_landmarks = hand_landmarks_list[idx]
-        handedness = handedness_list[idx]
-
-        # Draw the hand landmarks.
-        hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-        hand_landmarks_proto.landmark.extend([
-            landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks])
-        solutions.drawing_utils.draw_landmarks(
-            annotated_image,
-            hand_landmarks_proto,
-            solutions.hands.HAND_CONNECTIONS,
-            solutions.drawing_styles.get_default_hand_landmarks_style(),
-            solutions.drawing_styles.get_default_hand_connections_style())
-
-        # Get the top left corner of the detected hand's bounding box.
-        height, width, _ = annotated_image.shape
-        x_coordinates = [landmark.x for landmark in hand_landmarks]
-        y_coordinates = [landmark.y for landmark in hand_landmarks]
-        text_x = int(min(x_coordinates) * width)
-        text_y = int(min(y_coordinates) * height) - MARGIN
-
-        # Draw handedness (left or right hand) on the image.
-        cv2.putText(annotated_image, f"{handedness[0].category_name}",
-                    (text_x, text_y), cv2.FONT_HERSHEY_DUPLEX,
-                    FONT_SIZE, HANDEDNESS_TEXT_COLOR, FONT_THICKNESS, cv2.LINE_AA)
-    return annotated_image
-
-def show_list(position, save_frames, x, y, z):
-    '''Show List Function'''
-    if save_frames == 0:
-        print("List is empty")
-        return 0
-    # show each saved frame information
-    for row in range(0, save_frames+1, 1):
-        print(f"Frame{row}")
-        # show each positions, x,y,z value
-        for col in range(0, points, 1):
-            print(f"{position[col]} x={x[row][col]}, y={y[row][col]}, z={z[row][col]}")
-
-
 
 # set option
 options = HandLandmarkerOptions(
@@ -93,37 +29,34 @@ options = HandLandmarkerOptions(
 # The landmarker is initialized
 landmarker = HandLandmarker.create_from_options(options)
 
-# define output point and key point(wrist, <finger>_tip, <finger>_PIP, <finger>_MCP)
-position = [
-    "0 - wrist",
-    "1 - thumb_cmc", "2 - thumb_mcp", "3 - thumb_ip", "4 - thumb_tip",
-    "5 - index_finger_mcp", "6 - index_finger_pip", "7 - index_finger_dip", "8 - index_finger_tip",
-    "9 - middle_finger_mcp", "10 - middle_finger_pip", "11 - middle_finger_dip", "12 - middle_finger_tip",
-    "13 - ring_finger_mcp", "14 - ring_finger_pip", "15 - ring_finger_dip", "16 - ring_finger_tip",
-    "17 - pinky_finger_mcp", "18 - pinky_finger_pip", "19 - pinky_finger_dip", "20 - pinky_finger_tip"
-]
-
 # define x,y,z for saving key point
 points = 21
-x=[]
-y=[]
-z=[]
-hprx=[]
-hpry=[]
-hprz=[]
-for i in range(0,points,1):
+hprx = []
+hpry = []
+hprz = []
+for i in range(0, points, 1):
     hprx.append(0)
     hpry.append(0)
     hprz.append(0)
 
 # define flags
-written_flag = False
+execute_flag = False
+list_flag = False
 
 # define save_frames
 save_frames = 0
 
 # call HandPatternRecognition class
-hpr = HandPatternRecognition(hprx, hpry,hprz)
+hpr = HandPatternRecognition(hprx, hpry, hprz)
+
+# call HandPoseEstimationShow class
+hpes = HandPoseEstimationShow(points)
+
+# call and set inferencing module
+infer_model_path = 'C:/pywork/model/openvino.xml'
+device_name = 'CPU'
+input_shape = np.zeros((220, 453, 3))
+infer = ImageInferencing(infer_model_path, device_name, input_shape)
 
 # Use OpenCV’s VideoCapture to start capturing from the webcam.
 cap = cv2.VideoCapture(0)
@@ -144,17 +77,11 @@ while True:
     mp_image = mp.Image(image_format = mp.ImageFormat.SRGB,
                         data = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
 
-    '''
-    # Send live image data to perform hand landmarks detection.
-    # The results are accessible via the `result_callback` provided in
-    # the `HandLandmarkerOptions` object.
-    # The hand landmarker must be created with the live stream mode.
-    landmarker.detect_async(mp_image, time.time_ns() // 1_000_000)
-    '''
+    # detect hand landmark
     hand_landmarker_result = landmarker.detect(mp_image)
 
     # show detection_result to visible
-    annotated_image = draw_landmarks_on_image(mp_image.numpy_view(), hand_landmarker_result)
+    annotated_image = hpes.draw_landmarks_on_image(mp_image.numpy_view(), hand_landmarker_result)
     cv2.imshow("Result", cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
 
     # if detect hand, start HandPatternRecognition
@@ -164,33 +91,70 @@ while True:
             hprx[i] = hand_landmarker_result.hand_landmarks[0][i].x
             hpry[i] = hand_landmarker_result.hand_landmarks[0][i].y
             hprz[i] = hand_landmarker_result.hand_landmarks[0][i].z
-        hpr.set3DPosition(hprx, hpry, hprz)
+        hpr.set_3d_position(hprx, hpry, hprz)
 
         # calculate node angles
-        hpr.getNodesAngle()
+        hpr.get_node_angle()
 
         # detect ptrn
-        hpr.getNowPattern()
+        now_pattern = hpr.get_current_pattern()
 
-        # save key point at list if written_flag is True
-        if written_flag == True:
-            x.append(hprx[8])
-            y.append(hpry[8])
-            z.append(hprz[8])
-            save_frames += 1
+        # pick out mode pattern for avoiding scattering
+        mode_pattern = hpr.check_switch_pattern(now_pattern)
+
+        # status[0:stop, 1:write, 2:enter, 3:erase]
+        if execute_flag is False and mode_pattern == 0:
+            execute_flag = True
+            print("stop")
+
+        # execute each status
+        # writing action
+        if execute_flag is True and mode_pattern == 1:
+            # saving points save in x,y,z
+            print("writing")
+            execute_flag = True
+
+            # list_flag up
+            list_flag = True
+
+        # enter action
+        elif execute_flag is True and mode_pattern == 2:
+            if list_flag is True:
+                # list to xlxs
+                print("enter")
+                execute_flag = False
+
+                # list_flag down
+                list_flag = False
+
+            else:
+                print("List is empty. Please draw number or sign")
+                execute_flag = False
+
+        # erase action
+        elif execute_flag is True and mode_pattern == 3:
+            if list_flag is True:
+                # erase list
+                print("erase list")
+                execute_flag = False
+
+                # list_flag down
+                list_flag = False
+
+            else:
+                # erase picture or move index
+                print("erase picture")
+                execute_flag = False
 
     # show process time and fps
     process_time = time.time() - start_time
     FPS = 1 / process_time
-    print(f"process_time = {process_time:.4f}s, FPS = {FPS:.2f}")
+    # print(f"process_time = {process_time:.4f}s, FPS = {FPS:.2f}")
 
     # if pressed 'q', end capture
     key = cv2.waitKey(1) & 0xFF
     if key == ord('q'):
         break
-    elif key == ord('c'):
-        written_flag = not written_flag
-        print(f"written_flag = {written_flag}")
 
-# show list
-show_list(position, save_frames, x, y, z)
+cap.release()
+cv2.destroyAllWindows()
