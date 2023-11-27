a4292f9d6cb03048a68dddaa3b2c3438a5c34337
pjb8051 <woqud2180@gmail.com>
2023-11-27 19:17:00 +0900

    Update python code and README.md by pjb8051

--- UI/README.md
+++ UI/README.md
@@ -4,50 +4,72 @@ We are going to establish UART connection between a STM32 board and another devi
 
 The LCD board diplays an array of numbers and letters that shows where the cursor is
 
+# How to run
+1. Installation IDE
+<br>
+[STM32cubeIDE install][stm32] (Window / Linux / Mac)
+
+[stm32]: https://www.st.com/en/development-tools/stm32cubeide.html#get-software
+
+
+2. Setup completed
+
+    `You can find information about the settings below`
+
+3. STM32 code Run
+
+![](/images/run.png)
+
+4. Connect Bluetooth with PC
+
+5. Python code run
+
+# Setup imformaiton
+`Warining : Need to check pin number location`
 1.  IDE Settings
-> * Board : stm32 NUCLEO-F429Zl
-> * IDE : STM32cubeIDE
+    * Board : stm32 NUCLEO-F429Zl
+    * IDE : STM32cubeIDE
 
-> 1-1 GPIO settings
+1-1 GPIO settings
 
-![GPIO_setting](https://github.com/pjb8051/BoardMaestro/assets/143582470/1b0f6fb3-e561-4f9c-9071-ec4fd2120ebd)
+![](/images/GPIO_setting.png)
 
->
-> PC0 = GPIO_EXTI0 / Pull-down
-> PC3 = GPIO_EXTI3 / Pull-down
+PC0 = GPIO_EXTI0 / Pull-down
 
-> 1-2 I2C settings
+PC3 = GPIO_EXTI3 / Pull-down
+
+1-2 I2C settings
 
 ![](/images/i2c_setting.png)
->
-> PB6 = SCL
-> PB9 = SDA
 
-> 1-3 UART settings
+PB6 = SCL
+PB9 = SDA
+
+1-3 UART settings
 
 ![](/images/uart_setting.png)
->
-> USART2 = Asynchronous
-> Baud Rate = 9600 Bits/s
 
-> 1-4 NVIC settings
+USART2 = Asynchronous
+Baud Rate = 9600 Bits/s
+
+1-4 NVIC settings
 
 ![](/images/nvic_setting.png)
->
-> Endable list : 
-> * EXTI line0 interrupt 
-> * EXTI line3 interrupt 
-> * I2C1 event interrupt 
-> * I2C1 error interrupt 
-> * USART2 global interrupt 
-> * USART3 global interrupt 
-> * EXTI line[15:10] interrupt
+
+Endable list : 
+    * EXTI line0 interrupt 
+    * EXTI line3 interrupt 
+    * I2C1 event interrupt 
+    * I2C1 error interrupt 
+    * USART2 global interrupt 
+    * USART3 global interrupt 
+    * EXTI line[15:10] interrupt
 
 2. Use Module
-> * I2C_LCD = LCD1602
-> * Bluetooth = HC-06
+    * I2C_LCD = LCD1602
+    * Bluetooth = HC-06
 
 3. Result
-<br>
+
 
 ![](/images/1.gif)
--- UI/ble_interface.py
+++ UI/ble_interface.py
@@ -2,47 +2,82 @@
 import bluetooth
 import time
 import threading
+import re
+from PyQt5.QtCore import QObject, pyqtSignal
 
-def bluetoothsetup():
-    serverMACAddress = '98:DA:60:07:A1:3A' #목표 주소
-    port = 1    #포트는 1번으로 사용하는 것이 일반적
-    socekt = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-    socekt.connect((serverMACAddress, port))
-    return socekt
-
-def send_data(socket):
-    while True:
-        message = input('')
-        if message.lower() == 'q':
-            socket.close()
-            break
-
-        # STM32로 메시지 전송
-        socket.send(message + "\r\n")
-
-def receive_data(socket):
-    while True:
-        # STM32로부터 데이터 수신
-        data = socket.recv(1024)
-        print("Cursor move:", data.decode())  # 수신된 데이터 출력
-
-def main():
-    socket = bluetoothsetup()
-
-    try:
-        send_thread = threading.Thread(target=send_data, args=(socket,))
-        receive_thread = threading.Thread(target=receive_data, args=(socket,))
-
-        send_thread.start()
-        receive_thread.start()
-
-        send_thread.join()
-        receive_thread.join()
+class BluetoothWorker(QObject):
+    
+    '''
+    Class that help Bluetooth connection
+    
+    Instance : 
+        data_receivec = string
         
-    except KeyboardInterrupt:
-        pass  # Ctrl+C를 눌러 프로그램을 중지해도 오류가 발생하지 않게 함
-    finally:
-        socket.close()  # Bluetooth 소켓 닫기
+    Method :
+        __init__() : NONE
+        callstring() : string
+        bluetoothsetup() : NONE
+        send_data() : string
+        receive_data() : string
+    '''
+    data_received = pyqtSignal(str)  
+    
+    def __init__(self, parent=None):
+        '''Init parent, connect bluetooth'''
+        super().__init__(parent)
+        self.socket = self.bluetoothsetup()
+        self.str_buf = ""
+        self.number = 0
+    
+    def callstring(self,str_buf):
+        '''receive string and store in the str_buf'''
+        self.str_buf = str_buf
+        
+    def bluetoothsetup(self):
+        '''setting MACaddress and port and open socekt'''
+        serverMACAddress = '98:DA:60:07:A1:3A' 
+        port = 1    
+        socekt = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        socekt.connect((serverMACAddress, port))
+        return socekt
 
-if __name__ == "__main__":
-    main()
+    def send_data(self):
+        '''
+        read string and send
+        
+        input : string from str_buf
+        output : send to socket
+        '''
+        while True:
+            text = self.str_buf  
+            print(text)
+            if text.strip(): 
+                try:
+                    self.socket.send(text + "\r\n")
+                except bluetooth.btcommon.BluetoothError as e:
+                    print("Bluetooth error:", e)  
+                    
+            else:
+                print("No text to send")
+            time.sleep(1)
+
+    def receive_data(self):
+        '''
+        receive string and store
+        
+        intput : data from socket and decoding
+        output : extract numbers from the received text and stores them in the self.number variable
+        '''
+        while True:
+             received_data = self.socket.recv(1024)
+             decoded_data = received_data.decode()
+
+             self.received_text = decoded_data
+             
+             extracted_number = re.search(r'\d+', self.received_text).group()
+
+             if extracted_number:
+                self.number = int(extracted_number)
+                print(self.number)
+             else:
+                print("No number found in the text.")
