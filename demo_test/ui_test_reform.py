e45639aa17cdbbba651fe02df80a297049a9f422
roby238 <roby238_dev@naver.com>
2024-06-02 19:40:32 +0900

    [UI] update UI

--- demo_test/ui_test_reform.py
+++ demo_test/ui_test_reform.py
@@ -8,9 +8,10 @@ from pathlib import Path
 
 import sys
 import os
+import psutil
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-from PyQt5.QtCore import QTimer, QObject, pyqtSignal, pyqtSlot
+from PyQt5.QtCore import Qt, QTimer, QObject, pyqtSignal, pyqtSlot
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout, QPushButton, QTextEdit
 from PyQt5.QtGui import QFont
@@ -28,15 +29,12 @@ class App(QWidget):
     def __init__(self, w, h):
         super().__init__()
         self.title = 'Hand Draw Formula'
-        self.left = 10
-        self.top = 50
-        self.width = w
-        self.height = h
+        self.left, self.top = 10, 50
+        self.width, self.height = w, h
+        self.init_UI() 
 
         self.cap = cv2.VideoCapture(0)
 
-        self.initUI() 
-        
         # define x,y,z for saving key point
         self.points = 21
         self.hprx = []
@@ -48,8 +46,7 @@ class App(QWidget):
             self.hprz.append(0)
 
         # define 8_x,y,z for making image
-        self.x_8 = []
-        self.y_8 = []
+        self.x_8, self.y_8 = [], []
 
         # define flags and string_buf
         self.execute_flag = False
@@ -74,10 +71,8 @@ class App(QWidget):
  
         self.fps = FPS(mean_nb_frames=30)
         
-        self.nb_pd_inferences = 0
-        self.nb_lm_inferences = 0
-        self.glob_pd_rtrip_time = 0
-        self.glob_lm_rtrip_time = 0
+        self.nb_pd_inferences, self.nb_lm_inferences = 0, 0
+        self.glob_pd_rtrip_time, self.glob_lm_rtrip_time = 0, 0
 
         # call HandPatternRecognition class
         self.hpr = HandPatternRecognition(self.hprx, self.hpry, self.hprz, 9)
@@ -98,14 +93,8 @@ class App(QWidget):
         self.str_buf = ""
         self.str = ""
         self.str2 = f'counter: {self.preprocessing.result_counter}'
-
-        # read the frame from webcam and define
-        #_, frame = self.cap.read()
-        #self.frame_width = frame.shape[0]
-        #self.frame_height = frame.shape[1]
-
     
-    def initUI(self):
+    def init_UI(self):
         self.ratio_w, self.ratio_h = self.width / 1920, self.height / 1000
         
         #font size
@@ -115,11 +104,9 @@ class App(QWidget):
         self.label1 = QLabel(self)
         self.label1.move(10, 10)
         self.label1.resize(int(1280 * self.ratio_w), int(960 * self.ratio_h))
-
         self.label2 = QLabel(self)
         self.label2.move(int(1350 * self.ratio_w), int(20 * self.ratio_h))
         self.label2.resize(int(450 * self.ratio_w), int(450 * self.ratio_h))
-
         self.label3 = QLabel('Current formula  ', self)
         self.label3.move(int(1350 * self.ratio_w), int(500 * self.ratio_h))
         self.label4 = QLabel('None                     ', self)
@@ -129,61 +116,45 @@ class App(QWidget):
         self.label6 = QLabel('INVAILD                  ', self)
         self.label6.move(int(1350 * self.ratio_w), int(850 * self.ratio_h))
 
-        self.set_font()
-
-
-        self.timer = QTimer(self)
-        self.timer.timeout.connect(self.update_images)
-        self.timer.start(5)
-
-        # Print some stats
-        #print(f"# palm detection inferences : {nb_pd_inferences}")
-        #print(f"# hand landmark inferences  : {nb_lm_inferences}")
-        #print(f"Palm detection round trip   : {glob_pd_rtrip_time/nb_pd_inferences*1000:.1f} ms")
-        #print(f"Hand landmark round trip    : {glob_lm_rtrip_time/nb_lm_inferences*1000:.1f} ms")    
-        
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
-        self.show()
-
-    def set_font(self):
-        font = QFont('Arial', self.font_size)  # 폰트 설정
-        # 모든 라벨에 폰트 설정을 적용합니다.
+        # font setting
+        font = QFont('Arial', self.font_size)
         for label in [self.label1, self.label2, self.label3, self.label4, self.label5, self.label6]:
             label.setFont(font)
 
+        # start timer for UI
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_images)
+        self.timer.start(5)
+ 
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.show()
             
     def update_images(self):
         # to calculate running time, save start time to start_time
         start_time = time.time()
-
-        # read the frame from webcam
-        #_, frame = self.cap.read()
         
         self.fps.update()
 
+        pid = os.getpid()
+        proc = psutil.Process(pid)
+        cpu_usage = proc.cpu_percent()
+        mem_usage = round(proc.memory_info()[0] / 2. ** 30, 2)
+
+        # read the frame from webcam
         ret, frame = self.cap.read()
         if not ret:
             print("[GUI] Could Not read frame...")
             self.timer.stop()
             return
         
-        # detect hand landmark
-        #hand_landmarker_result, annotated_image = self.hpe.get_hand_pose_result(frame, True)
- 
         frame_nn = frame.copy()
 
         self.height, self.width = frame.shape[:2]
-        #if self.ht.crop:
         self.ht.frame_size = min(self.height, self.width)
         dx = (self.width - self.ht.frame_size) // 2
         dy = (self.height - self.ht.frame_size) // 2
         resized_frame = frame[dy:dy+self.ht.frame_size, dx:dx+self.ht.frame_size]
-        #else:
-        #    self.ht.frame_size = max(self.height, self.width)
-        #    pad_h = int((self.ht.frame_size - self.height) / 2)
-        #    pad_w = int((self.ht.frame_size - self.width) / 2)
-        #    frame = cv2.copyMakeBorder(frame, pad_h, pad_h, pad_w, pad_w, cv2.BORDER_CONSTANT)
 
         # Resize image to NN square input shape
         frame_nn = cv2.resize(resized_frame, (self.ht.pd_w, self.ht.pd_h), interpolation=cv2.INTER_AREA)
@@ -199,7 +170,7 @@ class App(QWidget):
         self.ht.pd_postprocess(inference)
         self.ht.pd_render(annotated_frame)
         self.nb_pd_inferences += 1
- 
+     
         self.handedness = 0
 
         # Hand landmarks
@@ -217,17 +188,14 @@ class App(QWidget):
             if r.lm_score > self.ht.lm_score_threshold:
                 self.handedness += 1
 
-                #print(f"[HandPoseEstm] Landmarks: {r.landmarks}")
-
-        #print(f"[HandPoseEstm] lm_pass_count: {self.lm_pass_count}")
-
-        #if not self.ht.crop:
-            #annotated_frame = annotated_frame[pad_h:pad_h+self.height, pad_w:pad_w+self.width]
-
-        #cv2.imshow("video", annotated_frame)
-
         # if detect hand, start HandPatternRecognition
         if self.handedness > 0:
+            # Print some stats
+            print(f"# palm detection inferences : {self.nb_pd_inferences}")
+            print(f"# hand landmark inferences  : {self.nb_lm_inferences}")
+            print(f"Palm detection round trip   : {self.glob_pd_rtrip_time/self.nb_pd_inferences*1000:.1f} ms")
+            print(f"Hand landmark round trip    : {self.glob_lm_rtrip_time/self.nb_lm_inferences*1000:.1f} ms")    
+            
             # save point to HandPatternRecognition
             for i, r in enumerate(self.ht.regions):
                 src = np.array([(0, 0), (1, 0), (1, 1)], dtype=np.float32)
@@ -235,14 +203,10 @@ class App(QWidget):
                 mat = cv2.getAffineTransform(src, dst)
                 lm_xy = np.expand_dims(np.array([(l[0], l[1]) for l in r.landmarks]), axis=0)
                 lm_xy = np.squeeze(cv2.transform(lm_xy, mat)).astype(np.int32)
-                #print(f"[HandPtrnRecog] lm_xyz: {lm_xyz}")
                 #lm_xyz = np.squeeze(cv2.transform(lm_xyz, mat)).astype(np.float32)
                 for j in range(self.points):
                     self.hprx[j], self.hpry[j] = (lm_xy[j][0] + 1) / self.width, (lm_xy[j][1] + 1) / self.height
-            #print(f"[HandPtrnRecog] hprx: {self.hprx}")
-            #print(f"[HandPtrnRecog] hpry: {self.hpry}")
-            #print(f"[HandPtrnRecog] hprz: {self.hprz}")
-            
+             
             self.hpr.set_3d_position(self.hprx, self.hpry, self.hprz)
 
             # pick out mode pattern for avoiding scattering
@@ -315,7 +279,6 @@ class App(QWidget):
                     self.list_flag = False
 
                 else:
-
                     # erase picture or move index
                     if self.preprocessing.result_counter != 0:
                         self.preprocessing.result_counter -= 1
@@ -330,14 +293,15 @@ class App(QWidget):
 
                     self.execute_flag = False
 
-        
         # Load images from file or camera
         annotated_frame = cv2.resize(annotated_frame,(int(1280 * self.ratio_w), int(960 * self.ratio_h)))
         self.fps.display(annotated_frame, orig=(int((1280 * 0.72) * self.ratio_w), 50), color=(240,180,100))
         
         # show detection_result to visible
-        cv2.putText(annotated_frame, self.str, (5, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0), 5)
-        cv2.putText(annotated_frame, self.str2, (5, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0), 1)
+        cv2.putText(annotated_frame, self.str, (5, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0), 5)
+        cv2.putText(annotated_frame, self.str2, (5, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0), 2) 
+        cv2.putText(annotated_frame, f"CPU: {cpu_usage:.2f}%", (int((1280 * 0.714) * self.ratio_w), 100), cv2.FONT_HERSHEY_SIMPLEX, 2, (240, 180, 100), 2)
+        cv2.putText(annotated_frame, f"MEM: {mem_usage:.2f}%", (int((1280 * 0.709) * self.ratio_w), 150), cv2.FONT_HERSHEY_SIMPLEX, 2, (240, 180, 100), 2)
 
         h, w, c = annotated_frame.shape
         img1 = QImage(annotated_frame.data, w, h, w * c, QImage.Format_BGR888)
@@ -366,53 +330,19 @@ class App(QWidget):
             self.label6.setText(self.calc.eval_proc(self.str_buf))
         else:
             self.label6.setText(str(self.calc.eval_proc(self.str_buf)))
-
-        #self.worker.callstring(self.str_buf)  # BluetoothWorker 클래스의 callstring 메소드 호출하여 str_buf 전달
-        # show process time and fps
-        #process_time = time.time() - start_time
-        #FPS = 1 / process_time
-        #print(f"process_time = {process_time:.4f}s, FPS = {FPS:.2f}")
-     
-        key = cv2.waitKey(1)
-        if key == ord('q') or key == 27:
-             self.timer.stop()
-             return
-        elif key == 32:
-            # Pause on space bar
-            cv2.waitKey(0)
-        elif key == ord('1'):
-            self.ht.show_pd_box = not self.ht.show_pd_box
-        elif key == ord('2'):
-            self.ht.show_pd_kps = not self.ht.show_pd_kps
-        elif key == ord('3'):
-            self.ht.show_rot_rect = not self.ht.show_rot_rect
-        elif key == ord('4'):
-            self.ht.show_landmarks = not self.ht.show_landmarks
-        elif key == ord('5'):
-            self.ht.show_handedness = not self.ht.show_handedness
-        elif key == ord('6'):
-            self.ht.show_scores = not self.ht.show_scores
-        elif key == ord('7'):
-            self.ht.show_gesture = not self.ht.show_gesture
-
-    def close_windows(self):
-        self.cap.release()
-        cv2.destroyAllWindows()
-
+    
+    def keyPressEvent(self, e):
+        if e.key() == Qt.Key_Escape:
+            print("Esc pressed. PyQt owari...")
+            self.close()
+        elif e.key() == Qt.Key_Q:
+            print("q pressed. PyQt owari...")
+            self.close()
 
 if __name__ == "__main__":
     main = QApplication(sys.argv)
     main_rect = main.desktop().screenGeometry()
     width, height = main_rect.width(), main_rect.height()
     ex = App(width, height)
-    ex.close_windows
     sys.exit(main.exec_())
 
-
-'''
-img = cv2.cvtColor(test_image, cv2.COLOR_BGR2RGB)
-h, w, c = img.shape
-q_img = QImage(img.data, w, h, w * c, QImage.Format_RGB888)
-# QImage를 QPixmap으로 변환
-pixmap = QPixmap.fromImage(q_img)
-'''
